123efd426c989d63216a62b26d16ac6cf8b37f33
DESKTOP-R9TM19D\PC <namdp.uet@gmail.com>
2022-09-06 10:12:29 +0700

    up

--- profile/src/App.js
+++ profile/src/App.js
@@ -18,6 +18,14 @@ function App() {
           Learn React
         </a>
       </header>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
     </div>
   );
 }
